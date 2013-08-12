e13694d9e6eea72663946033380537ce7034e525
George K. Thiruvathukal <gkt@cs.luc.edu>
2013-08-11 21:12:01 -0500

    update to logging output of self-deploy.sh script

--- self-deploy.sh
+++ self-deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "<<deploy $(date)>>" && \
-	hg incoming && hg pull && hg update --clean && \
+echo "<<< $(date) >>>"
+echo
+hg incoming && hg pull && hg update --clean && \
 	jekyll build && \
 	rsync -av --delete --exclude .htaccess \
 	./_site/ \
 	course@courseclouds.com:./sites/markup.cs.courseclouds.com/
+echo
